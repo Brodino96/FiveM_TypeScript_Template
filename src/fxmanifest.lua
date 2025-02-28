7950c708395018e6f689049e25a9cc98fa74ac3b
Vincent Acanfora <brodino96@gmail.com>
2025-02-28 15:01:04 +0100

    feat: implemented locales

--- src/fxmanifest.lua
+++ src/fxmanifest.lua
@@ -2,10 +2,10 @@ fx_version "cerulean"
 game "gta5"
 
 author "Brodino"
-description "Generic description"
+description ""
 version "0.1"
 
 server_scripts { "dist/server.js" }
 client_scripts { "dist/client.js" }
 
-files { "config.json" }
+files { "config.json", "locales/*" }
