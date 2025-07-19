7b6f06b7f3f8effd46cad019316b6ee97ca80e19
Vincent Acanfora <brodino96@gmail.com>
2025-07-19 23:19:31 +0200

    refactor: init

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,1 +1,9 @@
-import { Config } from "../shared/config"
+import Config from "../shared/config"
+import Logger from "../shared/logger"
+
+function init() {
+    Logger.info("This is the client!")
+    Logger.info(`The current configuration is: ${Config}`)
+}
+
+init()
