1332253f1eb80e7eb5bfda3d3d1a920785fb3047
Vincent Acanfora <brodino96@gmail.com>
2025-07-19 23:21:37 +0200

    refactor: locales are now exported as object feat: implemented locales validation

--- src/shared/locales.ts
+++ src/shared/locales.ts
@@ -1,9 +1,13 @@
-import { Config } from "../shared/config"
+import { readdirSync } from "fs"
+import Config from "./config"
 
 //@ts-ignore
 const jsonData = LoadResourceFile(GetCurrentResourceName(), `/locales/${Config.locale}.json`)
 const Locale = JSON.parse(jsonData)
 
-export default function L(index: string): string {
-    return Locale[index]
+export function getLocalesOptions(): string[] {
+    const path: string = GetResourcePath(GetCurrentResourceName())
+    return readdirSync(path, { encoding: "utf-8" })
 }
+
+export default Locale
