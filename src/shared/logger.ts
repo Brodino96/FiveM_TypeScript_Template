36d57c6dd91f1b90b091eb99c7c0769945c3e99e
Vincent Acanfora <brodino96@gmail.com>
2025-07-19 23:21:50 +0200

    refactor: better logic

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,19 +1,25 @@
-import { Config } from "./config"
+import Config from "./config"
 
-export class Logger {
-    public async error(txt: string, bypass?: boolean) {
-        if (Config.debugMode || bypass) {
-            console.log(`[ERROR] ${txt}`)
-        }
+export default class Logger {
+
+    public static error(txt: any, bypass?: boolean) {
+        if (!Config.debugMode || !bypass) return
+        console.error(txt)
+        console.log(`[ERROR] ${txt}`)
     }
-    public async success(txt: string, bypass?: boolean) {
-        if (Config.debugMode || bypass) {
-            console.log(`[SUCCESS] ${txt}`)
-        }
+
+    public static success(txt: any, bypass?: boolean) {
+        if (!Config.debugMode || !bypass) return
+        console.log(txt)
     }
-    public async info(txt: string, bypass?: boolean) {
-        if (Config.debugMode || bypass) {
-            console.log(`[INFO] ${txt}`)
-        }
+    
+    public static info(txt: any, bypass?: boolean) {
+        if (!Config.debugMode || !bypass) return
+        console.info(txt)
+    }
+    
+    public static warn(txt: any, bypass?: boolean) {
+        if (!Config.debugMode || !bypass) return
+        console.warn(txt)
     }
 }
