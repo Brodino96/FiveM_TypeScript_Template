6c542f536590d1d58dfe5f66c51ce11e9af4307f
Brodino <brodino96@gmail.com>
2025-07-20 09:30:21 +0200

    fix: now pulls from the correct path

--- src/server/utils/locales.ts
+++ src/server/utils/locales.ts
@@ -1,14 +1,16 @@
 import { readdirSync } from "fs"
-import Config from "./config"
+import { getConfig } from "./config"
+
 
 //@ts-ignore
-const jsonData = LoadResourceFile(GetCurrentResourceName(), `/locales/${Config.locale}.json`)
+const jsonData = LoadResourceFile(GetCurrentResourceName(), `/locales/${getConfig().locale}.json`)
 const Locale = JSON.parse(jsonData)
 
 export function getLocalesOptions(): string[] {
     //@ts-ignore
     const path: string = GetResourcePath(GetCurrentResourceName())
-    return readdirSync(path, { encoding: "utf-8" })
+    const files = readdirSync(`${path}/locales`, { encoding: "utf-8" })
+    return files.map(name => name.replace(/\.json$/, ""))
 }
 
 export default Locale
