71b625a7ff29ac97470ff9337c512634770661b0
Brodino <brodino96@gmail.com>
2025-07-20 02:27:54 +0200

    refactor: new Logger class

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,25 +1,53 @@
-import Config from "./config"
+export enum LogLevel {
+    debug = 0,
+    info = 1,
+    warn = 2,
+    error = 3,
+    nothing = 4
+}
 
-export default class Logger {
+export class Logger {
 
-    public static error(txt: any, bypass?: boolean) {
-        if (!Config.debugMode || !bypass) return
-        console.error(txt)
-        console.log(`[ERROR] ${txt}`)
+    private readonly layer: LogLevel
+
+    constructor(layer: LogLevel) {
+        this.layer = layer
     }
 
-    public static success(txt: any, bypass?: boolean) {
-        if (!Config.debugMode || !bypass) return
-        console.log(txt)
+    private display(level: LogLevel, ...args: any) {
+        if (this.layer <= level) return
+        switch (level) {
+            case LogLevel.debug:
+                console.debug(...args)
+                break
+
+            case LogLevel.info:
+                console.info(...args)
+                break
+
+            case LogLevel.warn:
+                console.warn(...args)
+                break
+
+            case LogLevel.error:
+                console.error(...args)
+                break
+        }
+    }
+
+    public debug(...args: any) {
+        this.display(LogLevel.debug, ...args)
     }
     
-    public static info(txt: any, bypass?: boolean) {
-        if (!Config.debugMode || !bypass) return
-        console.info(txt)
+    public info(...args: any) {
+        this.display(LogLevel.info, ...args)
     }
     
-    public static warn(txt: any, bypass?: boolean) {
-        if (!Config.debugMode || !bypass) return
-        console.warn(txt)
+    public warn(...args: any) {
+        this.display(LogLevel.warn, ...args)
+    }
+    
+    public error(...args: any) {
+        this.display(LogLevel.error, ...args)
     }
 }
