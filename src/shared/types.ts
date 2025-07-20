a755d2d68b6bed331371c4b2bafc031d16b488ba
Brodino <brodino96@gmail.com>
2025-07-20 10:43:52 +0200

    fix: correct import statement for defaultLocale type in types.ts

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,3 +1,3 @@
-import defaultLocale from "../../script/locales/en.json"
+import type defaultLocale from "../../script/locales/en.json";
 
-export type LocaleType = typeof defaultLocale
+export type LocaleType = typeof defaultLocale;
